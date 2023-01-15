4b150857cfd6d97bf80d6d99d8917ebb8337bd16
Marion Perrié <marion.perrie@gmail.com>
2023-01-15 20:03:19 +0100

    Update[header, footer, banner] : add styles
    Add a margin for footer and header so that its the same in every page
    Update the banner component : put image in background and add a before element for filter

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
-//Import image
-import homeBanner from '../../assets/banner.png'
+// //Import image
+// import homeBanner from '../../assets/banner.png'
 
 //Import Style
 import './Banner.scss'
@@ -9,8 +9,6 @@ function Banner() {
   return (
     <div>
       <div className="banner">
-        <img className="banner__img" src={homeBanner} alt="" />
-        <div className="banner__background"></div>
         <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
       </div>
     </div>
