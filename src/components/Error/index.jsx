62e8368fde04109a2e827a05575c42903d7b04a5
Marion Perrié <marion.perrie@gmail.com>
2023-01-17 17:36:37 +0100

    Update[error] : add style to error component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,19 @@
+//Import tools
+import { Link } from 'react-router-dom'
+
+//Import style
+import './Error.scss'
+
 function Error() {
   return (
-    <div>
-      <p>Message d'erreur : cette page n'existe pas !</p>
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/">
+        <p className="active-link">Retourner sur la page d’accueil</p>
+      </Link>
     </div>
   )
 }
