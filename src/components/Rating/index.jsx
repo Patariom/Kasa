df7ca1f38586530d713b245a87ab870fa4cbd4c7
Marion Perrié <marion.perrie@gmail.com>
2023-01-21 21:33:54 +0100

    Feat[rating.jsx] : Add new rating component
    Import stars icon (one orange and one grey)
    Style the component

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,35 @@
+//Import tools
+import React from 'react'
+
+//Import images
+import greyStar from '../../assets/rate_star.svg'
+import orangeStar from '../../assets/rate_star_active.svg'
+
+//Component
+function Rating({ rating }) {
+  const range = [1, 2, 3, 4, 5]
+  console.log(rating)
+  return (
+    <div className="rating-container">
+      {range.map((rangeElem) =>
+        rating >= rangeElem ? (
+          <img
+            className="rating-container.stars"
+            src={orangeStar}
+            alt=""
+            key={rangeElem.toString()}
+          />
+        ) : (
+          <img
+            className="rating-container.stars"
+            src={greyStar}
+            alt=""
+            key={rangeElem.toString()}
+          />
+        )
+      )}
+    </div>
+  )
+}
+
+export default Rating
